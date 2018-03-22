aa42321fa180d7b54bd0b2abd1b9eac0ff8ffb9b
Corentin Bect <corentin.bect@gmail.com>
2018-03-22 22:39:34 +0100

    merge

--- src/main/java/com/epul/oeuvres/controle/MultiControleur.java
+++ src/main/java/com/epul/oeuvres/controle/MultiControleur.java
@@ -1,37 +1,21 @@
 package com.epul.oeuvres.controle;
 
 //import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.epul.oeuvres.dao.Service;
+import com.epul.oeuvres.meserreurs.MonException;
+import com.epul.oeuvres.metier.AdherentEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
-//import com.epul.metier.*;
-//import com.epul.meserreurs.*;
-
-
-
-import com.epul.oeuvres.dao.Service;
-import com.epul.oeuvres.meserreurs.*;
-import com.epul.oeuvres.metier.*;
-
-
-
-
-
-
-
-import org.springframework.ui.Model;
 import org.springframework.web.servlet.view.RedirectView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-import java.util.*;
+//import com.epul.metier.*;
+//import com.epul.meserreurs.*;
 
 ///
 /// Les méthode du contrôleur répondent à des sollicitations
@@ -42,21 +26,21 @@ public class MultiControleur {
 
 //	private static final Logger logger = LoggerFactory.getLogger(MultiControleur.class);
 
-	@RequestMapping(value = "listerAdherent.htm")
-	public ModelAndView afficherLesStages(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		String destinationPage;
-		try {
-			// HttpSession session = request.getSession();
-			Service unService = new Service();
-			request.setAttribute("mesAdherents", unService.consulterListeAdherents());
-			destinationPage = "listerAdherent";
-		} catch (MonException e) {
-			request.setAttribute("MesErreurs", e.getMessage());
-			destinationPage = "Erreur";
+    @RequestMapping(value = "listerAdherent.htm")
+    public ModelAndView afficherLesStages(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String destinationPage;
+        try {
+            // HttpSession session = request.getSession();
+            Service unService = new Service();
+            request.setAttribute("mesAdherents", unService.consulterListeAdherents());
+            destinationPage = "listerAdherent";
+        } catch (MonException e) {
+            request.setAttribute("MesErreurs", e.getMessage());
+            destinationPage = "Erreur";
 
-		}
-		return new ModelAndView(destinationPage);
-	}
+        }
+        return new ModelAndView(destinationPage);
+    }
 
     @RequestMapping(value = "listerOeuvres.htm")
     public ModelAndView listerLesOeuvres(HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -74,27 +58,27 @@ public class MultiControleur {
         return new ModelAndView(destinationPage);
     }
 
-	@RequestMapping(value = "insererAdherent.htm")
-	public ModelAndView insererAdherent(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    @RequestMapping(value = "insererAdherent.htm")
+    public ModelAndView insererAdherent(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-		String destinationPage = "";
-		try {
-			AdherentEntity unAdherent = new AdherentEntity();
-			unAdherent.setNomAdherent(request.getParameter("txtnom"));
-			unAdherent.setPrenomAdherent(request.getParameter("txtprenom"));
-			unAdherent.setVilleAdherent(request.getParameter("txtville"));
-			Service unService = new Service();
-			unService.insertAdherent(unAdherent);
-		} catch (Exception e) {
-			request.setAttribute("MesErreurs", e.getMessage());
-			destinationPage = "Erreur";
-		}
-		destinationPage = "home";
-		return new ModelAndView(destinationPage);
-	}
+        String destinationPage = "";
+        try {
+            AdherentEntity unAdherent = new AdherentEntity();
+            unAdherent.setNomAdherent(request.getParameter("txtnom"));
+            unAdherent.setPrenomAdherent(request.getParameter("txtprenom"));
+            unAdherent.setVilleAdherent(request.getParameter("txtville"));
+            Service unService = new Service();
+            unService.insertAdherent(unAdherent);
+        } catch (Exception e) {
+            request.setAttribute("MesErreurs", e.getMessage());
+            destinationPage = "Erreur";
+        }
+        destinationPage = "home";
+        return new ModelAndView(destinationPage);
+    }
 
-	@RequestMapping(value = "modifierAdherent.htm")
-	public ModelAndView afficherAdherent(HttpServletRequest request, HttpServletResponse response) {
+    @RequestMapping(value = "modifierAdherent.htm")
+    public ModelAndView afficherAdherent(HttpServletRequest request, HttpServletResponse response) {
         String destinationPage = "";
         try {
             // HttpSession session = request.getSession();
@@ -109,76 +93,74 @@ public class MultiControleur {
         return new ModelAndView(destinationPage);
     }
 
-	@RequestMapping(value = "updateAdherent.htm")
-	public RedirectView updateAdherent(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    @RequestMapping(value = "updateAdherent.htm")
+    public RedirectView updateAdherent(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-		String destinationPage = "";
-		try {
+        String destinationPage = "";
+        try {
             Service unService = new Service();
             AdherentEntity unAdherent = unService.adherentById(Integer.parseInt(request.getParameter("id")));
             unAdherent.setNomAdherent(request.getParameter("nom"));
             unAdherent.setPrenomAdherent(request.getParameter("prenom"));
             unAdherent.setVilleAdherent(request.getParameter("ville"));
-			unService.updateAdherent(unAdherent);
-		} catch (Exception e) {
-			request.setAttribute("MesErreurs", e.getMessage());
-			destinationPage = "Erreur";
-		}
-		destinationPage = "home";
+            unService.updateAdherent(unAdherent);
+        } catch (Exception e) {
+            request.setAttribute("MesErreurs", e.getMessage());
+            destinationPage = "Erreur";
+        }
+        destinationPage = "home";
         return new RedirectView("listerAdherent.htm");
-	}
+    }
 
-	@RequestMapping(value = "supprimerAdherent.htm")
-	public RedirectView supprimerAdherent(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    @RequestMapping(value = "supprimerAdherent.htm")
+    public RedirectView supprimerAdherent(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-	    AdherentEntity adherent = null;
-		try {
-		    Service service = new Service();
-			service.deleteAdherent(Integer.parseInt(request.getParameter("id")));
-		} catch (Exception e) {
-			request.setAttribute("MesErreurs", e.getMessage());
-		}
-		//return new RedirectView(String.valueOf(adherent == null) + "><");
-		return new RedirectView("listerAdherent.htm");
-	}
+        AdherentEntity adherent = null;
+        try {
+            Service service = new Service();
+            service.deleteAdherent(Integer.parseInt(request.getParameter("id")));
+        } catch (Exception e) {
+            request.setAttribute("MesErreurs", e.getMessage());
+        }
+        //return new RedirectView(String.valueOf(adherent == null) + "><");
+        return new RedirectView("listerAdherent.htm");
+    }
 
-	@RequestMapping(value = "ajouterAdherent.htm")
-	public ModelAndView ajouterAdherent(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    @RequestMapping(value = "ajouterAdherent.htm")
+    public ModelAndView ajouterAdherent(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-		String destinationPage = "";
-		try {
-			destinationPage = "ajouterAdherent";
-		} catch (Exception e) {
-			request.setAttribute("MesErreurs", e.getMessage());
-			destinationPage = "rreur";
-		}
+        String destinationPage = "";
+        try {
+            destinationPage = "ajouterAdherent";
+        } catch (Exception e) {
+            request.setAttribute("MesErreurs", e.getMessage());
+            destinationPage = "rreur";
+        }
 
-		return new ModelAndView(destinationPage);
-	}
+        return new ModelAndView(destinationPage);
+    }
 
-	// /
-	// / Affichage de la page d'accueil
-	// /
-	@RequestMapping(value = "index.htm", method = RequestMethod.GET)
-	public ModelAndView Afficheindex(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		return new ModelAndView("home");
-	}
+    // /
+    // / Affichage de la page d'accueil
+    // /
+    @RequestMapping(value = "index.htm", method = RequestMethod.GET)
+    public ModelAndView Afficheindex(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return new ModelAndView("home");
+    }
 
-	// /
-		// / Affichage de la page d'accueil
-		// /
-		@RequestMapping(value = "/", method = RequestMethod.GET)
-		public ModelAndView Afficheindex2(HttpServletRequest request, HttpServletResponse response) throws Exception {
-			return new ModelAndView("home");
-		}
-	// /
-	// / Affichage de la page d'accueil
-	// /
-	@RequestMapping(value = "erreur.htm", method = RequestMethod.GET)
-	public ModelAndView AfficheErreur(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		return new ModelAndView("Erreur");
-	}
-	
-	
+    // /
+    // / Affichage de la page d'accueil
+    // /
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView Afficheindex2(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return new ModelAndView("home");
+    }
 
+    // /
+    // / Affichage de la page d'accueil
+    // /
+    @RequestMapping(value = "erreur.htm", method = RequestMethod.GET)
+    public ModelAndView AfficheErreur(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return new ModelAndView("Erreur");
+    }
 }
