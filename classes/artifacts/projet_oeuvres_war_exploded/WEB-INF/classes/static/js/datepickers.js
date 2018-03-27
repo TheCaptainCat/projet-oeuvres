3c0b0e13e164765120a1ea4d16cc0dbb96427b57
Gaetan Chambouvet <gaetan.chambouvet@orange.fr>
2018-03-27 21:03:03 +0100

    Datepickers

---
+++ classes/artifacts/projet_oeuvres_war_exploded/WEB-INF/classes/static/js/datepickers.js
@@ -0,0 +1,30 @@
+$.datepicker.setDefaults(
+    $.extend(
+        {'dateFormat':'dd-mm-yy'},
+        $.datepicker.regional['nl']
+    )
+);
+
+var createDatePicker = function() {
+    $( ".bookingDate" ).datepicker({
+        altField: "#datepicker",
+        closeText: 'Fermer',
+        prevText: 'Précédent',
+        nextText: 'Suivant',
+        currentText: 'Aujourd\'hui',
+        monthNames: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+        monthNamesShort: ['Janv.', 'Févr.', 'Mars', 'Avril', 'Mai', 'Juin', 'Juil.', 'Août', 'Sept.', 'Oct.', 'Nov.', 'Déc.'],
+        dayNames: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+        dayNamesShort: ['Dim.', 'Lun.', 'Mar.', 'Mer.', 'Jeu.', 'Ven.', 'Sam.'],
+        dayNamesMin: ['D', 'L', 'M', 'M', 'J', 'V', 'S'],
+        weekHeader: 'Sem.',
+        dateFormat: 'yy-mm-dd',
+        firstDay: 1
+    });
+};
+
+createDatePicker();
+
+$(document).on('focus',".bookingDate", function(){
+    createDatePicker();
+});
